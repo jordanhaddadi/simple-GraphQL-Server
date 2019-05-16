76318b8be4a828f3df625da2ba0bc9d3c4516b25
Jordan <jordanhaddadi@gmail.com>
2019-05-16 11:21:17 -0400

    installed dependencies and connected mlab

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-var express = require('express');
-var express_graphql = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+const PORT = process.env.PORT || "4000";
+const db = "mongodb://jordan:pretty1@ds157516.mlab.com:57516/talkpretty";
+
+// Connect to MongoDB with Mongoose.
+mongoose
+  .connect(
+    db,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
 // GraphQL schema
-var schema = buildSchema(`
+const schema = buildSchema(`
     type Query {
         message: String
     }
 `);
 // Root resolver
-var root = {
+const root = {
     message: () => 'Hello World!'
 };
 // Create an express server and a GraphQL endpoint
-var app = express();
+const app = express();
 app.use('/graphql', express_graphql({
     schema: schema,
     rootValue: root,
